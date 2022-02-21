4579935ae1f290961c2dbb3f5c3452dea87cca86
Ahmad-Ali123 <ahmadali7901634@gmail.com>
2022-02-21 15:22:09 -0500

    webville toons is up and runnig

--- javascript/playlist.js
+++ javascript/playlist.js
@@ -1,9 +1,21 @@
 window.onload = init;
 function init() {
-var button = document.getElementById("addButton");
-button.onclick = handleButtonClick;
+  var button = document.getElementById("addButton");
+  button.onclick = handleButtonClick;
+  loadPlaylist();
 }
 
 function handleButtonClick() {
-  alert("Button was clicked!"); 
+  var textInput = document.getElementById("songTextInput");
+  var songName = textInput.value;
+  var li = document.createElement("li")
+  li.innerHTML = songName;
+  var ul = document.getElementById("playlist");
+  ul.appendChild(li);
+  save(songName)
+  if (songName == "") {
+       alert("please enter a song");
+  } else {
+      alert("Adding " + songName);
+  }
 }
