3b1744d401f9fef184efae3bc2030f036e79d685
Ahmad-Ali123 <ahmadali7901634@gmail.com>
2022-02-24 08:54:07 -0500

    still trying to fix validation

--- javascript/playlist.js
+++ javascript/playlist.js
@@ -5,6 +5,14 @@ function init() {
   loadPlaylist();
 }
 
+function addValidation(){
+	var loc = window.location.href;
+	var HTMLvalidLinkStr = 'http://validator.w3.org/check?uri=' + loc;
+	var CSSvalidLinkStr = 'http://jigsaw.w3.org/css-validator/validator?uri=' + loc + '?profile=css3';
+	document.getElementById("vLink1").setAttribute("href", HTMLvalidLinkStr);
+	document.getElementById("vLink2").setAttribute("href", CSSvalidLinkStr);
+}
+
 function handleButtonClick() {
   var textInput = document.getElementById("songTextInput");
   var songName = textInput.value;
